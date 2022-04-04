dc2664f3d14265aa0f5e7519acf9583ee358bb51
Trey Dockendorf <tdockendorf@osc.edu>
2022-04-04 13:59:14 -0400

    Only remove what apptainer documents to remove

--- manifests/singularity.pp
+++ manifests/singularity.pp
@@ -3,44 +3,10 @@
 class apptainer::singularity {
   assert_private()
 
-  if $apptainer::install_method == 'source' {
-    file { '/etc/bash_completion.d/singularity':
-      ensure => 'absent',
-    }
-    file { '/etc/singularity':
-      ensure  => 'absent',
-      purge   => true,
-      recurse => true,
-      force   => true,
-    }
-    -> file { '/usr/local/sbin/singularity-mconfig.sh':
-      ensure => 'absent',
-    }
-    -> file { '/usr/bin/run-singularity':
-      ensure => 'absent',
-    }
-    -> file { '/usr/libexec/singularity':
-      ensure  => 'absent',
-      purge   => true,
-      recurse => true,
-      force   => true,
-    }
-    -> file { '/var/singularity':
-      ensure  => 'absent',
-      purge   => true,
-      recurse => true,
-      force   => true,
-    }
-    -> file { '/opt/singularity':
-      ensure  => 'absent',
-      purge   => true,
-      recurse => true,
-      force   => true,
-    }
-    ~> exec { 'rm-singularity-source':
-      path        => '/usr/bin:/bin:/usr/sbin:/sbin',
-      command     => 'rm -rf /opt/singularity-3*',
-      refreshonly => true,
-    }
+  file { '/etc/singularity':
+    ensure  => 'absent',
+    purge   => true,
+    recurse => true,
+    force   => true,
   }
 }
