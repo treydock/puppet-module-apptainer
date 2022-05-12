14203cd0b8fd19d458332c17fd716426885dec8d
treydock <tdockendorf@osc.edu>
2022-05-12 12:08:54 -0400

    Uninstall plugins before apptainer upgrades to avoid errors during upgrade (#2)

--- spec/defines/plugin_spec.rb
+++ spec/defines/plugin_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
 describe 'apptainer::plugin' do
-  on_supported_os.each do |os, facts|
+  on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) do
-        facts
+        os_facts
       end
 
       let :title do
@@ -16,6 +16,8 @@ describe 'apptainer::plugin' do
       end
 
       let(:pre_condition) { "class { 'apptainer': install_method => 'source' }" }
+
+      it { is_expected.not_to contain_exec('apptainer-plugin-uninstall-for-upgrade-github.com/apptainer/apptainer/log-plugin') }
 
       it do
         is_expected.to contain_exec('apptainer-plugin-compile-github.com/apptainer/apptainer/log-plugin').with(
@@ -70,6 +72,20 @@ describe 'apptainer::plugin' do
             'Exec[apptainer-plugin-reinstall-github.com/apptainer/apptainer/log-plugin]',
           ],
         )
+      end
+
+      context 'when upgrading apptainer' do
+        let(:facts) { os_facts.merge(apptainer_version: '1.0.1') }
+        let(:pre_condition) { "class { 'apptainer': version => '1.0.2', install_method => 'source' }" }
+
+        it do
+          is_expected.to contain_exec('apptainer-plugin-uninstall-for-upgrade-github.com/apptainer/apptainer/log-plugin').with(
+            path: '/usr/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin',
+            command: 'apptainer plugin uninstall github.com/apptainer/apptainer/log-plugin',
+            onlyif: "apptainer plugin list | grep 'github.com/apptainer/apptainer/log-plugin'",
+            before: 'Class[Apptainer::Install::Source]',
+          )
+        end
       end
 
       context 'when ensure => absent' do
